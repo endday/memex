a9168cffb2c10bda0ac96c97cf394d3b341b2629
sparkleMing <1207120891@qq.com>
2026-03-28 17:22:41 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -796,6 +796,7 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
     if (mounted) {
       setState(() {
         _isInputOpen = false;
+        _sharedDraft = null;
       });
     }
 
